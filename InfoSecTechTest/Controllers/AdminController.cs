9c346836fc0413ff3b942a7323b1b8206d734b9f
Chris Dare <chris.dare@just-eat.com>
2016-06-28 09:28:05 +0100

    added auth

---
+++ InfoSecTechTest/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InfoSecTechTest.Models;
+using System.Web.Security;
+
+namespace InfoSecTechTest.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "Form is not valid; please review and try again.";
+                return View("Login");
+            }
+
+            if (login.Username == "user" && login.Password == "password")
+                FormsAuthentication.RedirectFromLoginPage(login.Username, true);
+
+            ViewBag.Error = "Credentials invalid. Please try again.";
+            return View("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Articles");
+        }
+
+
+    }
+}
---
+++ InfoSecTechTest/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InfoSecTechTest.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace InfoSecTechTest.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
